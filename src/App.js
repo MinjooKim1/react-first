77325908fb9f313bef1c09b6fa8dc4e49fb273eb
Minjoo Kim <haileykim206@gmail.com>
2024-08-14 08:59:46 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./App.css";
 import Box from "./Component/Box";
+import paper from "./img/paper.png";
+import scissors from "./img/scissors.png";
+import rock from "./img/rock.png";
 
 const choice = {
   rock: {
@@ -18,20 +21,54 @@ const choice = {
 };
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
   };
+
+  const judgement = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    if (user.name == computer.name) {
+      return "tie";
+    } else if (user.name === "Rock")
+      return computer.name === "Scissors" ? "win" : "lose";
+    else if (user.name === "Scissors")
+      return computer.name === "Paper" ? "win" : "lose";
+    else if (user.name === "Paper")
+      return computer.name === "Rock" ? "win" : "lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만들어준다.
+    console.log("item array", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random Value", randomItem);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+
   return (
     <div>
       <div className="main">
-        <Box title="YOU" item={userSelect} />
-        {/* <Box title="COMPUTER" /> */}
+        <Box title="YOU" item={userSelect} result={result} />
+        <Box title="COMPUTER" item={computerSelect} result={result} />
       </div>
 
       <div className="main">
-        <button onClick={() => play("scissors")}>가위</button>
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+        <button onClick={() => play("scissors")}>
+          <img src={scissors} alt="scissors" width="50px" />
+        </button>
+        <button onClick={() => play("rock")}>
+          <img src={rock} alt="rock" width="50px" />
+        </button>
+        <button onClick={() => play("paper")}>
+          <img src={paper} alt="paper" width="50px" />
+        </button>
       </div>
     </div>
   );
--- src/Component/Box.js
+++ src/Component/Box.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 const Box = (props) => {
-  console.log("props", props);
+  let result;
+  if (
+    props.title === "COMPUTER" &&
+    props.result !== "tie" &&
+    props.result !== ""
+  ) {
+    result = props.result === "win" ? "lose" : "win";
+  } else {
+    result = props.result;
+  }
+
   return (
-    <div className="box">
-      <h1>{props.title}</h1>
+    <div className={`box ${result}`}>
+      <h1 className="head">{props.title}</h1>
+      <h3 className="subHead">{props.item && props.item.name}</h3>
       <img className="item-img" src={props.item && props.item.img} />
-      <h2>win</h2>
+      <h2>{result}</h2>
     </div>
   );
 };
